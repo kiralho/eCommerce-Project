76080567d23c43c490677df17fb931f610a357c5
kiralho <vinicius_torii@hotmail.com>
2018-11-02 18:49:21 +0000

    Updated seed script to populate the Province table reading a json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
+require 'pp'
+
+ProductInOrder.destroy_all
+Product.destroy_all
+Order.destroy_all
+Category.destroy_all
+TransactionStatus.destroy_all
+User.destroy_all
+Province.destroy_all
+AdminUser.destroy_all
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-Province.create(name: "Manitoba", tax_rate: "0.08")
+json = File.read('provinces.json')
+parsed_json = JSON.parse(json)
 
-User.create(name: "vinicius", email: "teste@teste.com", address: "rua teste 1323", province_id: 1)
+parsed_json["message"].each do |province, info|
+    name = info["name"]
+    rate = info["tax"]
+    Province.create(name: name, tax_rate: rate)
+end
 
-Category.create(name: "computador")
+# Province.create(name: "Manitoba", tax_rate: "0.08")
 
-Product.create(name: "laptop", price: 500, description: "teste teste teste", category_id: 1)
+# User.create(name: "vinicius", email: "teste@teste.com", address: "rua teste 1323", province_id: 1)
 
-TransactionStatus.create(name: "Pending")
+# Category.create(name: "computador")
 
-Order.create(user_id: 1, transaction_status_id: 1)
+# Product.create(name: "laptop", price: 500, description: "teste teste teste", category_id: 1)
 
-ProductInOrder.create(order_id: 1, product_id: 1)
+# TransactionStatus.create(name: "Pending")
+
+# Order.create(user_id: 1, transaction_status_id: 1)
+
+# ProductInOrder.create(order_id: 1, product_id: 1)
