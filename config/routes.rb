c6e90931785bce8c477e4d56cdf4826f0ee85ae8
kiralho <vinicius_torii@hotmail.com>
2018-11-06 22:00:57 +0000

    Created controller for Page, configured routes and show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'page/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :page, only: [:show]
   resources :category, only: [:index, :show]
   resources :product, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
