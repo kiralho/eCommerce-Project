8fad3e8e5fd671662194812d59ce02a0b9d48906
kiralho <vinicius_torii@hotmail.com>
2018-11-13 17:21:25 +0000

    Modified search controller and views. Tested and it's working fine

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,13 @@
 class SearchController < ApplicationController
+  def index
+
+  end
+
+  def results
+    @category = Category.find_by(name: params[:q])
+    unless @category.nil?
+     @products = Product.where(category_id: @category.id)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   resources :page, only: [:show]
   resources :category, only: [:index, :show]
   resources :product, only: [:index, :show]
+  resources :search, only: [:index] do
+    collection do
+       get 'results'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'product#index'
 end
