4016e9381f7e2ccf11e8f00272dc1bf94e6abcd7
kiralho <vinicius_torii@hotmail.com>
2018-11-15 19:06:15 +0000

    Implemented search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,20 @@
 class SearchController < ApplicationController
   def index
-
+    @categories = Category.all
   end
 
   def results
-    @category = Category.find_by(name: params[:q])
-    unless @category.nil?
-     @products = Product.where(category_id: @category.id)
+    @categories = Category.all
+    @teste = params[:q]
+
+    if params[:c] == "" && params[:q] == ""
+      @products = Product.all
+    elsif params[:c] == ""
+      @products = Product.where("name LIKE ? OR description LIKE ?", "%#@teste%", "%#@teste%")
+    else
+      @category = Category.find(params[:c])
+      @products = Product.where(category_id: @category.id)
+      @products = @products.where("name LIKE ? OR description LIKE ?", "%#@teste%", "%#@teste%")
     end
   end
 
